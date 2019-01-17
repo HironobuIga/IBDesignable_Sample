f77d6ade0080723a39d1aefd46b8557e5973e2c9
iganin <hironobu.iga.developer@gmail.com>
2019-01-17 17:13:34 +0900

    [add] IBDesignableサンプル用のプロジェクト作成

---
+++ IBDesignable_Sample/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBDesignable_Sample
+//
+//  Created by HironobuIga on 2019/01/17.
+//  Copyright © 2019 iganin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
