310279a17cf285cfcb6d92c083e8d25be85c1c22
iganin <hironobu.iga.developer@gmail.com>
2019-01-20 12:41:19 +0900

    [add]画面表示とモデルクラスとの紐付けを実施、画面表示修正

--- IBDesignable_Sample/Screens/ViewController.swift
+++ IBDesignable_Sample/Screens/ViewController.swift
@@ -12,6 +12,16 @@ final class ViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet private weak var scrollView: UIScrollView!
     
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var mainNameLabel: UILabel!
+    @IBOutlet weak var mainCategoryLabel: UILabel!
+    @IBOutlet weak var mainOutlineLabel: UILabel!
+    
+    @IBOutlet var subBaseViews: [UIView]!
+    @IBOutlet var subImageViews: [UIImageView]!
+    @IBOutlet var subNameLabels: [UILabel]!
+    @IBOutlet var subCategoryLabels: [UILabel]!
+    @IBOutlet var subOutlineLabels: [UILabel]!
     
     // MARK: - Property
     override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
@@ -19,23 +29,48 @@ final class ViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupBaseView()
-        setupView()
+        setupBaseParts()
+        setupMainParts()
+        setupSubParts()
     }
     
-    // MARK: - Function
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        scrollView.contentInset.bottom = view.safeAreaInsets.bottom + 30.0
+    }
     
 }
 
 // MARK: - Private Function
 private extension ViewController {
-    func setupView() {
-    }
-    
-    func setupBaseView() {
+    func setupBaseParts() {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         scrollView.contentInsetAdjustmentBehavior = .never
+    }
+
+    func setupMainParts() {
+        let mainData = SampleDataFactory.makeMainData()
+        headerImageView.image = UIImage(named: mainData.imageName)
+        mainNameLabel.text = mainData.name
+        mainCategoryLabel.text = mainData.category
+        mainOutlineLabel.text = mainData.outlineText
+    }
+    
+    func setupSubParts() {
+        let subDataList = SampleDataFactory.makeSubDataList()
+        subDataList.enumerated().forEach { (index, data) in
+            subImageViews[index].image = UIImage(named: data.imageName)
+            subNameLabels[index].text = data.name
+            subCategoryLabels[index].text = data.category
+            subOutlineLabels[index].text = data.outlineText
+        }
+        
+        subBaseViews.forEach { view in
+            view.layer.cornerRadius = 6.0
+            view.layer.borderWidth = 1.0
+            view.layer.borderColor = UIColor.darkGray.cgColor
+        }
     }
 }
 
